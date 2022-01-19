7e34fef5a54910629ac1447e4e8312796f052e7e
FSquirrelzz <Yoavbn111@gmail.com>
2022-01-19 18:41:26 +0200

    wip

---
+++ ClassApplication/client/src/app/guards/auth.guard.ts
@@ -0,0 +1,25 @@
+/*import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { ToastrService } from "ngx-toastr";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn:'root'
+})
+export class AuthGuard  implements CanActivate {
+  constructor(private accountService:accountService,
+    private toastr:ToastrService
+    ) { }
+  canActivate(): Observable<boolean>{
+    return this.accountService.currentUser$.pipe(
+      map(user=>{
+        if (user) return true;
+        this.toastr.error('You must be signed in to access this page.'));
+        return false;
+      })
+    )
+  }
+
+}
+
+// uncomment when im caught up to 19/01, missing too much to make this work
